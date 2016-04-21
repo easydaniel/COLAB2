00b6bfd1c5333ca5028af545559cdec6582a474a
Kevin Chen <me@kevchentw.com>
2016-04-21 11:48:38 +0800

    decoder first implementant

--- Decoder.v
+++ Decoder.v
@@ -10,7 +10,7 @@
 //--------------------------------------------------------------------------------
 
 module Decoder(
-  instr_op_i,
+    instr_op_i,
 	RegWrite_o,
 	ALU_op_o,
 	ALUSrc_o,
@@ -38,5 +38,53 @@ reg            Branch_o;
 
 
 //Main function
+// op code
+// 000: load/save word
+// 001: branch equal
+// 010: r type
+// 011: ADD
+// 100: SLT
+// 101: LUI
+// 110: OR
+// 111: AND
 
+always @(instr_op_i) begin
+    case (instr_op_i)
+        0: begin // r type
+            ALU_op_o <= 3'b010;
+            ALUSrc_o <= 0;
+            RegWrite_o <= 1;
+            RegDst_o <= 1;
+            Branch_o <= 0;
+        end
+        4: begin // branch
+            ALU_op_o <= 3'b001;
+            ALUSrc_o <= 0;
+            RegWrite_o <= 0;
+            RegDst_o <= 0; //x
+            Branch_o <= 1;
+        end
+        8: begin // addi
+            ALU_op_o <= 3'b011;
+            ALUSrc_o <= 0;
+            RegWrite_o <= 1;
+            RegDst_o <= 0;
+            Branch_o <= 0;
+        end
+        9: begin // sltiu
+            ALU_op_o <= 3'b100;
+            ALUSrc_o <= 0;
+            RegWrite_o <= 0;
+            RegDst_o <= 1;
+            Branch_o <= 0;
+        end
+        default: begin
+            ALU_op_o <= 3'b000;
+            ALUSrc_o <= 0;
+            RegWrite_o <= 0;
+            RegDst_o <= 0;
+            Branch_o <= 0;
+        end
+    endcase
+end
 endmodule
